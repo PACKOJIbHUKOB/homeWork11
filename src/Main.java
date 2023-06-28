475546a9d8d37cce7ab35b9635a9b4d779a32974
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-06-28 16:22:03 +0300

    выполнены первая я вторая задача

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {   task1();
         task2();
@@ -7,12 +9,58 @@ public class Main {
         task6();
         task7();
         task8();}
+    public static int checkLeapYear(int year ) {
+       if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+           year = 1;
+       }
+       return year;
+    }
+    public static int checkOS (int clientOS) {
+        int iOS = 0, android = 1;
+        if (clientOS ==android){
+            clientOS=1;
+        }else if (clientOS==iOS){
+            clientOS=0;
+        }
+        return clientOS;
+    }
+    public static int checkYearPhone (int yearPhone){
+        int currentYear = LocalDate.now().getYear();
+        if (yearPhone==currentYear){
+            yearPhone=1;
+        }else {yearPhone=0;}
+        return yearPhone;
+    }
     private static void task1() {
-        System.out.println("задача 1");}
-    private static void task2() {
-        System.out.println("задача 2");
+        System.out.println("задача 1");
+        int year = 2000;
+        int leapYears = checkLeapYear(year);
+        if (leapYears == 1) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год неявляется високосным");
+        }
 
     }
+    private static void task2() {
+        System.out.println("задача 2");
+        int clientOS = 0, clientDeviceYear = 2023;
+        int phoneOS = checkOS(clientOS);
+        int yearPhone = checkYearPhone(clientDeviceYear);
+        if (phoneOS==1&&yearPhone==1) {
+            System.out.println("Установите приложения для Android по ссылке");
+        } else if (phoneOS==1&&yearPhone!=1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (phoneOS==0&&yearPhone==1) {
+            System.out.println("Установите приложения для iOS по ссылке");
+        } else if (phoneOS==0&&yearPhone==0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else{
+            System.out.println("Ваша операционная система не поддерживаеться");
+        }
+
+    }
+
     private static void task3() {
         System.out.println("задача 3");}
     private static void task4() {
