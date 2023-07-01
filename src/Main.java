80f1aad98426c25a4d270e526885d7f16d29d49a
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-07-01 14:14:11 +0300

    замечания исправлены

--- src/Main.java
+++ src/Main.java
@@ -19,43 +19,54 @@ public class Main {
         }
 
     }
-    public static void checkYearsAndOS (int clientOS, int clientDeviceYear ) {
+    public static int checkYearsAndOS (int clientOS, int clientDeviceYear ) {
         int currentYear = LocalDate.now().getYear();
         int androidOS =1, iOS=0;
         if(clientOS==androidOS&&clientDeviceYear>=currentYear){
-            System.out.println("Установите приложения для Android по ссылке");
+           return 1;
         } else if (clientOS==androidOS&&clientDeviceYear<currentYear) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            return 2;
         } else if (clientOS==iOS&&clientDeviceYear>=currentYear) {
-            System.out.println("Установите приложения для iOS по ссылке");
+            return 3;
         } else if (clientOS==iOS&&clientDeviceYear<currentYear) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }else {System.out.println("Ваша операционная система не поддерживаеться");}
-
+            return 4;
+        }else {return -1;}
     }
     private static void task2() {
         System.out.println("задача 2");
-        checkYearsAndOS(1,2018);
+        int clientOs = 3;
+        int clientDeviceYear= 2021;
+        int versionApplication= checkYearsAndOS(clientOs,clientDeviceYear);
+        //названия переменных в коде и методы не обязанны совпадать.
+        if (versionApplication==1){
+            System.out.println("Установите приложение для Android по ссылке");
+        } else if (versionApplication==2) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (versionApplication==3) {
+            System.out.println("Установите приложение для iOS по ссылке");
+        } else if (versionApplication==4) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }else{
+            System.out.println("Ваша операционная система не поддерживаеться");}
+
     }
     private static int checkDeliveryDay (int distance){
-        int day = 0;
         if (distance<20) {
-             day ++;
+             return 1;
         } else if (20<=distance&&distance<60) {
-             day +=2;
+             return 2;
         } else if (60<=distance&&distance<=100) {
-            day +=3;
+            return 3;
         }else {
-            day+=4;
+            return 4;
         }
-        return day;
     }
     private static void task3() {
         System.out.println("задача 3");
-        int deliveryDistance = 60;
-        int deliverydays = checkDeliveryDay(deliveryDistance);
-        if(deliverydays<=3){
-            System.out.println("Потребуется дней: " + (deliverydays));
+        int deliveryDistance = 47;
+        int deliveryDay = checkDeliveryDay(deliveryDistance);
+        if(deliveryDay<=3){
+            System.out.println("Потребуется дней: " + (deliveryDay));
         }else {
             System.out.println("доставки нет");}
 
